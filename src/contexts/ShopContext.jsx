0590e42f6b7dacff45aaae6551a9601c7e69c38a
Sitt Thiha Naing <sixxxxthihanaing@gmail.com>
2024-05-07 21:46:35 +0700

    add remove function

--- src/contexts/ShopContext.jsx
+++ src/contexts/ShopContext.jsx
@@ -5,22 +5,44 @@ export const ShopContext = createContext(null);
 
 const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState([]);
-  console.log("🚀 ~ ShopContextProvider ~ cartItems:", cartItems)
+  console.log("🚀 ~ ShopContextProvider ~ cartItems:", cartItems);
 
   const addToCart = (product) => {
-    const existingItemIndex = cartItems.findIndex((item) => item.id === product.id);
+    const existingItemIndex = cartItems.findIndex(
+      (item) => item.id === product.id
+    );
 
-    if (existingItemIndex !== -1) {
+    if (existingItemIndex === 0) {
       const updatedCartItems = [...cartItems];
+
       updatedCartItems[existingItemIndex].quantity =
         updatedCartItems[existingItemIndex].quantity + 1 || 1;
-      setCartItems(updatedCartItems); 
+      setCartItems(updatedCartItems);
     } else {
       setCartItems([...cartItems, { ...product, quantity: 1 }]);
     }
   };
 
-  const contextValue = { allProduct, addToCart };
+  const decreaseQuantity = (product) => {
+    const existingItemIndex = cartItems.findIndex(
+      (item) => item.id === product.id
+    );
+
+    if (existingItemIndex === 0) {
+      const updatedCartItems = [...cartItems];
+      if (updatedCartItems[existingItemIndex].quantity > 1) {
+        updatedCartItems[existingItemIndex].quantity -= 1;
+        setCartItems(updatedCartItems);
+      } else {
+        const updatedCartItems = cartItems.filter(
+          (item) => item.id !== product.id
+        );
+        setCartItems(updatedCartItems);
+      }
+    }
+  };
+
+  const contextValue = { allProduct, addToCart, decreaseQuantity };
 
   return (
     <ShopContext.Provider value={contextValue}>
